190310d98a786555b67d424d77c2d3fc737083c5
Ayush Kumar <ayush.k@outlook.com>
2022-07-23 13:13:15 -0400

    Commented methods that the program will use.

--- PegSolitaire.java
+++ PegSolitaire.java
@@ -1,5 +1,28 @@
+/*
+ * Program Name: Peg Solitaire
+ * Author: Ayush Kumar
+ * Description: This Java program allows you to play Peg Solitaire on the CLI.
+*/
+
 public class PegSolitaire {
     public static void main(String[] args) {
         
     }
+
+    // int readValidInt(Scanner in, String prompt, int min, int max)
+
+    // char[][] createBoard(char[][] board)
+
+    // void displayBoard(char[][] board)
+
+    // int[] readValidMove(Scanner in, char[][] board)
+
+    // boolean isValidMove(char[][] board, int row, int column, int direction)
+
+    // char[][] performMove(char[][] board, int row, int column, int direction)
+
+    // int countPegsRemaining(char[][] board)
+
+    // int countMovesAvailable(char[][] board)
+    
 }
