fed936820140a3d08cabff69413542a462a9d1e6
Ayush Kumar <ayush.k@outlook.com>
2022-07-23 15:03:12 -0400

    Created and sucessfully tested readValidInt method.

--- PegSolitaire.java
+++ PegSolitaire.java
@@ -4,12 +4,37 @@
  * Description: This Java program allows you to play Peg Solitaire on the CLI.
 */
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class PegSolitaire {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        readValidInt(input, "Enter a number between 1 and 4: ", 1, 4);
     }
 
     // int readValidInt(Scanner in, String prompt, int min, int max)
+    public static int readValidInt(Scanner input, String prompt, int min, int max) {
+        int answer;
+        System.out.print(prompt);
+
+        while (true) {
+            try {
+                answer = input.nextInt();
+
+                if(answer >= min && answer <= max) {
+                    return answer;
+                } else {
+                    System.out.print("Please enter a number between " + min + " and " + max + ": ");
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Please enter a numerical value: ");
+                input.next();
+            }
+        }
+    }
 
     // char[][] createBoard(char[][] board)
 
@@ -24,5 +49,5 @@ public class PegSolitaire {
     // int countPegsRemaining(char[][] board)
 
     // int countMovesAvailable(char[][] board)
-    
+
 }
